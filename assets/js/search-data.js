8ebd553531207d4a5d932e88a4f96162435a2051
francescodibetta <95315820+francescodibetta@users.noreply.github.com>
2025-12-03 15:14:39 +0000

    Deploying to gh-pages from @ francescodibetta/francescodibetta.github.io@9eafd6ce71b25afe331fea1730acbfc7582b5459 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,11 +417,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
